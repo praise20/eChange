a7df78e850da715d554e5ee99e6d98601fc3a5f6
iykeafrica <ikechiucheagwu@gmail.com>
2020-10-12 16:44:15 +0100

    Manifest Intent Filter moved to OTPVerification Activity for test

---
+++ app/src/main/java/com/alc/echange/OTPVerificationActivity.java
@@ -0,0 +1,265 @@
+package com.alc.echange;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class OTPVerificationActivity extends AppCompatActivity {
+
+    private static final String TAG = "OTPVerificationActivity";
+    EditText mEditText1, mEditText2, mEditText3, mEditText4, mEditText5, mEditText6;
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    private Button mVerificationButton;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_verification);
+
+        editTextInitialization();
+        mVerificationButton = findViewById(R.id.verify_otp);
+        verificationButtonClick();
+        mAuth = FirebaseAuth.getInstance();
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential credential) {
+                String code = credential.getSmsCode();
+                signInWithPhoneAuthCredential(credential);
+                Toast.makeText(OTPVerificationActivity.this, "Verification completed!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId,
+                                   @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:" + verificationId);
+
+                // Save verification ID and resending token so we can use them later
+                mVerificationId = verificationId;
+                mResendToken = token;
+
+                // [START_EXCLUDE]
+                // Update UI
+                //updateUI(STATE_CODE_SENT);
+                // [END_EXCLUDE]
+            }
+        };
+
+        startPhoneNumberVerification();
+    }
+
+    private void verifyPhoneNumberWithCode(String verificationId, String code) {
+        // [START verify_with_code]
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        // [END verify_with_code]
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void startPhoneNumberVerification() {
+        // [START start_phone_auth]
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+23408067994494",        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+        // [END start_phone_auth]
+    }
+
+    private void verificationButtonClick() {
+
+        mVerificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (!TextUtils.isEmpty(mEditText1.getText().toString()) && !TextUtils.isEmpty(mEditText2.getText().toString())
+                        && !TextUtils.isEmpty(mEditText3.getText().toString()) && !TextUtils.isEmpty(mEditText4.getText().toString())) {
+
+                    String code = mEditText1.getText().toString() + mEditText2.getText().toString() +
+                            mEditText2.getText().toString() + mEditText2.getText().toString();
+
+                    verifyPhoneNumberWithCode(mVerificationId, code);
+
+                } else {
+                    Toast.makeText(OTPVerificationActivity.this, "One or more field is empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Toast.makeText(this, "" + currentUser, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "No current user", Toast.LENGTH_SHORT).show();
+            startPhoneNumberVerification();
+        }
+    }
+    // [END on_start_check_user]
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+
+                            FirebaseUser user = task.getResult().getUser();
+                            // [START_EXCLUDE]
+//                            updateUI(STATE_SIGNIN_SUCCESS, user);
+                            // [END_EXCLUDE]
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                                // [START_EXCLUDE silent]
+//                                mBinding.fieldVerificationCode.setError("Invalid code.");
+                                // [END_EXCLUDE]
+                            }
+                            // [START_EXCLUDE silent]
+                            // Update UI
+//                            updateUI(STATE_SIGNIN_FAILED);
+                            // [END_EXCLUDE]
+                        }
+                    }
+                });
+    }
+
+    public class GenericKeyEvent implements View.OnKeyListener {
+
+
+        private EditText currentView, previousView;
+        public GenericKeyEvent (EditText currentView, EditText previousView) {
+            this.currentView = currentView;
+            this.previousView = previousView;
+        }
+
+        @Override
+        public boolean onKey(View view, int i, KeyEvent keyEvent) {
+            if (keyEvent.getAction() == KeyEvent.ACTION_DOWN && i == KeyEvent.KEYCODE_DEL &&
+                    currentView.getId() != R.id.edit_text_one && currentView.getText().toString().isEmpty()) {
+                //If current is empty then previous EditText's number will also be deleted
+                previousView.setText(null);
+                previousView.requestFocus();
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class GenericTextWatcher implements TextWatcher {
+
+        private View currentView, nextView;
+
+        private GenericTextWatcher(View currentView, View nextView) {
+            this.currentView = currentView;
+            this.nextView = nextView;
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            // TODO Auto-generated method stub
+            String text = editable.toString();
+            switch (currentView.getId()) {
+                case R.id.edit_text_one:
+                case R.id.edit_text_two:
+                case R.id.edit_text_three:
+                case R.id.edit_text_four:
+                case R.id.edit_text_five:
+                    if (text.length() == 1)
+                        nextView.requestFocus();
+                    break;
+            }
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+            // TODO Auto-generated method stub
+        }
+    }
+
+    private void editTextInitialization() {
+        mEditText1 = findViewById(R.id.edit_text_one);
+        mEditText2 = findViewById(R.id.edit_text_two);
+        mEditText3 = findViewById(R.id.edit_text_three);
+        mEditText4 = findViewById(R.id.edit_text_four);
+        mEditText5 = findViewById(R.id.edit_text_five);
+        mEditText6 = findViewById(R.id.edit_text_six);
+
+        mEditText1.setInputType(InputType.TYPE_NULL);
+        mEditText1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mEditText1.setInputType(InputType.TYPE_CLASS_NUMBER);
+                mEditText1.requestFocus();
+                InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                mgr.showSoftInput(mEditText1, InputMethodManager.SHOW_FORCED);
+
+            }
+        });
+
+        mEditText1.addTextChangedListener(new GenericTextWatcher(mEditText1, mEditText2));
+        mEditText2.addTextChangedListener(new GenericTextWatcher(mEditText2, mEditText3));
+        mEditText3.addTextChangedListener(new GenericTextWatcher(mEditText3, mEditText4));
+        mEditText4.addTextChangedListener(new GenericTextWatcher(mEditText4, mEditText5));
+        mEditText5.addTextChangedListener(new GenericTextWatcher(mEditText5, mEditText6));
+        mEditText6.addTextChangedListener(new GenericTextWatcher(mEditText6, null));
+
+        //GenericKeyEvent here works for deleting the element and to switch back to previous EditText
+        //first parameter is the current EditText and second parameter is previous EditText
+        mEditText1.setOnKeyListener(new GenericKeyEvent(mEditText1, null));
+        mEditText2.setOnKeyListener(new GenericKeyEvent(mEditText2, mEditText1));
+        mEditText3.setOnKeyListener(new GenericKeyEvent(mEditText3, mEditText2));
+        mEditText4.setOnKeyListener(new GenericKeyEvent(mEditText4, mEditText3));
+        mEditText5.setOnKeyListener(new GenericKeyEvent(mEditText5, mEditText4));
+        mEditText6.setOnKeyListener(new GenericKeyEvent(mEditText6, mEditText5));
+    }
+}
