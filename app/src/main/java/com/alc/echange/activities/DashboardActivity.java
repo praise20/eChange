3a1c43cabf44092824992df86eea744b32e5c6b2
Emmanuel Macaulay <61067933+Oba-max22@users.noreply.github.com>
2020-10-14 17:56:33 +0100

    Update DashboardActivity.java
    Emmanuel updated dashboard activity manually

--- app/src/main/java/com/alc/echange/activities/DashboardActivity.java
+++ app/src/main/java/com/alc/echange/activities/DashboardActivity.java
@@ -10,5 +10,35 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        
+            Toolbar toolbar = findViewById(R.id.bottom_toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.dashboard_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.overview:
+                Toast.makeText(this, "This is the Overview menu", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.history:
+                Toast.makeText(this, "This is the History menu", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.fund_account:
+                Toast.makeText(this, "This is the fund account menu", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
